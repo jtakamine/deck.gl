b89425ff4e5d7e2acc637fe7c2f599e3bb9d0be2
jtakamine <justin@clover.com>
2018-03-16 01:31:28 -0700

    wall clock

--- examples/clover-latency/app.js
+++ examples/clover-latency/app.js
@@ -10,47 +10,11 @@ import {json as requestJson} from 'd3-request';
 // Set your mapbox token here
 const MAPBOX_TOKEN = process.env.MapboxAccessToken; // eslint-disable-line
 
-/*
-SELECT
-    lon AS lng,
-    lat,
-    count(*) AS weight,
-    sum(time_total_session - time_in_server) / count(*) AS latency,
-    quantile(0.5)(time_total_session - time_in_server) AS latency_p50,
-    quantile(0.75)(time_total_session - time_in_server) AS latency_p75,
-    quantile(0.9)(time_total_session - time_in_server) AS latency_p90,
-    quantile(0.95)(time_total_session - time_in_server) AS latency_p95,
-    quantile(0.99)(time_total_session - time_in_server) AS latency_p99,
-    quantile(0.999)(time_total_session - time_in_server) AS latency_p999
-FROM haproxy_log
-WHERE (status_code = 200)
-  AND NOT ((lat = 38)
-  AND (lon = -97))
-  AND NOT ((lat = 0) AND (lon = 0))
-  AND (backend_name = 'authserver')
-  AND is_pay
-GROUP BY
-    lon,
-    lat
-INTO OUTFILE 'test_authserver.csv'
-FORMAT CSVWithNames;
-*/
-const DATA_URL_1 =
-  // 'http://localhost:8080/test_all.csv';
-  'http://localhost:8080/test_authserver.csv';
-
-const DATA_URL_2 =
-  'http://localhost:8080/data_example_2.csv';
-
 const DATA_BASE_URL =
   'http://localhost:8080/data-puller/latency_data/';
 
-
 // const DATA_URL =
 //   'https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/3d-heatmap/heatmap-data.csv'; // eslint-disable-line
-
-var DATA_1 = null;
-var DATA_2 = null;
 
 var dataFiles = null;
 var numDataFiles = null;
@@ -79,26 +43,19 @@ class Root extends Component {
     });
   }
 
-  swapData() {
-    if (DATA_1 != null && DATA_2 != null) {
-      if (data_index === 0) {
-        data_index = 1;
-        this.setState(DATA_1)
-      } else {
-        data_index = 0;
-        // this.setState(DATA_1)
-        this.setState(DATA_2)
-      }
-      console.log(data_index);
-    }
-  }
-
   loadData(dataFileId) {
     const dataFile = dataFiles[dataFileId];
+    const unix = Number(dataFile.split(/[_.]+/)[1]);
+    const date = new Date(unix * 1000);
     const url = DATA_BASE_URL + dataFile;
-    console.log(url);
+
     requestCsv(url, (error, response) => {
       console.log('dataFileId ' + dataFileId);
+      console.log('unixTime ' + unix);
+      console.log('date ' + date);
+
+      document.getElementById('header').innerHTML = 'Clover Traffic -- ' + date;
+
       if (!error) {
         const data = response.map(d => [Number(d.lng), Number(d.lat), Number(d.weight), Number(d.latency_p99)]);
         this.setState({data});
